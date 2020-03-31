c74d4df74d2c0af9166e459cf034fe9524ba00fb
Gerben Oolbekkink <g.j.w.oolbekkink@gmail.com>
2020-03-31 10:04:27 +0200

    Update extension code

--- ext/content.js
+++ ext/content.js
@@ -1,28 +1,20 @@
 (() => {
     const pdfFrame = document.querySelector('.d2l-fileviewer-pdf-pdfjs');
-    console.log(pdfFrame.dataset);
     const pdfLocation = pdfFrame.dataset.location;
-    const base = chrome.runtime.getURL("index.html?buffer=true");
-    const iframeUrl = base + "?url=" + encodeURI(window.location.origin + pdfLocation);
-
-    console.log(iframeUrl);
-
     const iframe = document.createElement('iframe');
-    iframe.src = base;
-    iframe.style.width = "100%";
-    iframe.style.height = "960px";
-    iframe.addEventListener('load', () => {
 
+    const responseFuture =
         fetch(window.location.origin + pdfLocation)
             .then(response => response.blob())
             .then(blob => blob.arrayBuffer())
-            .then((buffer) => {
-                console.log('posting message');
 
-                iframe.contentWindow.postMessage(buffer, '*')
-            });
+    iframe.src = chrome.runtime.getURL("index.html?buffer=true");
+    iframe.style.width = "100%";
+    iframe.style.height = "960px";
+    iframe.addEventListener('load', async () => {
+        const buffer = await responseFuture;
+        iframe.contentWindow.postMessage(buffer, '*');
     });
 
     pdfFrame.replaceWith(iframe);
-
 })();
